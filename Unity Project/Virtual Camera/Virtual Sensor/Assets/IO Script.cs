251bfe1d17d8cd547cce1700dc0439431ce41ee3
Dylan Shah <dylan.shah@outlook.com>
2025-06-05 03:46:00 -0700

    Started logging raycasts to file

--- Unity Project/Virtual Camera/Virtual Sensor/Assets/IO Script.cs
+++ Unity Project/Virtual Camera/Virtual Sensor/Assets/IO Script.cs
@@ -1,26 +1,56 @@
-using UnityEngine;
+using System;
 using System.IO;
+using UnityEngine;
 
 public class IOScript : MonoBehaviour
 {
+    // TODO: replace hard coded file path
+    Camera unityCamera;
+    StreamWriter writer;
+    Ray ray;
+
+    Vector3 angle;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         print("start");
+        writer = File.CreateText("outputFile.txt");
+        unityCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
         print("loop");
-        // TODO: replace hard coded file path
-        using (
-            StreamWriter writer = File.AppendText(
-                "/home/robocat/Documents/Code/ASU/Summer Internship/Unity Project/file.txt"
+        writer.WriteLine("loop frame number: " + Time.frameCount);
+
+        angle = new Vector3(0, 0, 1); //Hardcoded forward for now
+        if (
+            Physics.Raycast(
+                new Ray(unityCamera.transform.position, angle),
+                out RaycastHit raycastHit
             )
         )
         {
-            writer.WriteLine("loop frame number: " + Time.frameCount);
+            String output =
+                raycastHit.collider.name
+                + " was hit, angle was "
+                + angle.ToString()
+                + ", distance was "
+                + raycastHit.distance;
+            print(output);
+            writer.WriteLine(output);
+        }
+
+        writer.Flush();
+    }
+
+    void OnDestroy()
+    {
+        if (writer != null)
+        {
+            writer.Close();
         }
     }
 }
