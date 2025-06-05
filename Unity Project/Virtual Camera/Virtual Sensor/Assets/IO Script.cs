54759804d97916538937e8abbd84c7aa3e4aaae7
Dylan Shah <dylan.shah@outlook.com>
2025-06-05 02:50:59 -0700

    Created output to file

--- Unity Project/Virtual Camera/Virtual Sensor/Assets/IO Script.cs
+++ Unity Project/Virtual Camera/Virtual Sensor/Assets/IO Script.cs
@@ -1,20 +1,26 @@
 using UnityEngine;
+using System.IO;
 
 public class IOScript : MonoBehaviour
 {
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         print("start");
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         print("loop");
-        
+        // TODO: replace hard coded file path
+        using (
+            StreamWriter writer = File.AppendText(
+                "/home/robocat/Documents/Code/ASU/Summer Internship/Unity Project/file.txt"
+            )
+        )
+        {
+            writer.WriteLine("loop frame number: " + Time.frameCount);
+        }
     }
-
 }
