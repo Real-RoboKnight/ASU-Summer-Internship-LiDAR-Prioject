e76a5817177086a3b7f31ddbfc1d2d3d0c2ca147
Dylan Shah <dylan.shah@outlook.com>
2025-06-05 01:24:17 -0700

    Added Unity Project

---
+++ Unity Project/Virtual Camera/Virtual Sensor/Assets/IO Script.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class IOScript : MonoBehaviour
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("start");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("loop");
+        
+    }
+
+}
