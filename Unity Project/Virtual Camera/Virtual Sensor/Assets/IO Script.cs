8b8cccb4905960e2bfe5d99a01a1d05a1904fede
Dylan Shah <dylan.shah@outlook.com>
2025-06-05 04:48:37 -0700

    Full scanning created
    On my machine, full pi/64 scan takes about 2-3 seconds.
    
    TODO: output data to CSV

--- Unity Project/Virtual Camera/Virtual Sensor/Assets/IO Script.cs
+++ Unity Project/Virtual Camera/Virtual Sensor/Assets/IO Script.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class IOScript : MonoBehaviour
 {
+    const double ANGULAR_RESOLUTION = Math.PI / 64;
+
     // TODO: replace hard coded file path
     Camera unityCamera;
     StreamWriter writer;
-    Ray ray;
-
-    Vector3 angle;
+    double theta = 0;
+    double phi = Math.PI;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,31 +20,58 @@ public class IOScript : MonoBehaviour
         unityCamera = Camera.main;
     }
 
-    // Update is called once per frame
+    Vector3 CalculateAngle(double theta, double phi)
+    {
+        // theta in [0, 2pi] and phi in [-pi. pi]
+        // phi and theta 0 is the +x vector. Increasing theta rotates counterclockwise on the xz plane. Increasing phi rotates counterclockwise up towards +y
+        // (cos(θ), sin(θ), sin(φ)).normalize
+
+        return new Vector3(
+            (float)Math.Sin(theta),
+            (float)Math.Cos(theta),
+            (float)Math.Sin(phi)
+        ).normalized;
+    }
+
+    // Update is called once per frame. Each frame we should do another point
     void Update()
     {
-        print("loop");
-        writer.WriteLine("loop frame number: " + Time.frameCount);
-
-        angle = new Vector3(0, 0, 1); //Hardcoded forward for now
-        if (
-            Physics.Raycast(
-                new Ray(unityCamera.transform.position, angle),
-                out RaycastHit raycastHit
-            )
-        )
+        for (phi = Math.PI; phi > -Math.PI; phi -= ANGULAR_RESOLUTION)
         {
-            String output =
-                raycastHit.collider.name
-                + " was hit, angle was "
-                + angle.ToString()
-                + ", distance was "
-                + raycastHit.distance;
-            print(output);
-            writer.WriteLine(output);
+            if (
+                Physics.Raycast(
+                    new Ray(unityCamera.transform.position, CalculateAngle(theta, phi)),
+                    out RaycastHit raycastHit
+                )
+            )
+            {
+                string output =
+                    raycastHit.collider.name
+                    + " was hit, theta was "
+                    + theta
+                    + ", rho was "
+                    + phi
+                    + ", distance was "
+                    + raycastHit.distance;
+                print(output);
+                writer.WriteLine(output);
+            }
+            else
+            {
+                string output = "Nothing was hit, theta was " + theta + ", rho was " + phi;
+                print(output);
+                writer.WriteLine(output);
+            }
         }
-
-        writer.Flush();
+        theta += ANGULAR_RESOLUTION;
+        if (theta > 2 * Math.PI)
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.ExitPlaymode();
+#else
+            Application.Quit();
+#endif
+        }
     }
 
     void OnDestroy()
